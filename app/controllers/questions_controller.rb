6e6f06d74846ed73ca74503daaebe478f16004a2
sergiososa <smatias.sosa@gmail.com>
2022-05-17 22:58:35 -0300

    stupider

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @response = params[:pepe]   #guardo lo que el usuario escribio en el input(capturado en el params)
+    if @response == "I am going to work"
+      @answer = "Great!"         # creo variable de instancia answer(la llamo desde answer.html)
+    elsif @response.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
